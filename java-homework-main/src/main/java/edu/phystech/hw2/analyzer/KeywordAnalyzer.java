b9e21730317d27a1ccbbbc8453e2d8a2f04a1b26
Munee-sharaldai <147613151+Munee-sharaldai@users.noreply.github.com>
2024-04-10 00:35:13 +0800

    Add files via upload

---
+++ java-homework-main/src/main/java/edu/phystech/hw2/analyzer/KeywordAnalyzer.java
@@ -0,0 +1,23 @@
+package edu.phystech.hw2.analyzer;
+
+import java.util.List;
+
+public abstract class KeywordAnalyzer implements TextAnalyzer {
+
+    private final List<String> keywords;
+    private final Label label;
+
+    public KeywordAnalyzer(List<String> keywords, Label label) {
+        this.keywords = keywords;
+        this.label = label;
+    }
+
+    @Override
+    public Label processText(String text) {
+        return null;
+    }
+
+    public List<String> getKeywords() {
+        return this.keywords;
+    }
+}
