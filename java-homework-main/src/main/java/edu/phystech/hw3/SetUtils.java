ec81f23e3285a52ee625f272b4669ecc05a75709
Munee-sharaldai <147613151+Munee-sharaldai@users.noreply.github.com>
2024-04-27 01:35:45 +0800

    Added HW3

---
+++ java-homework-main/src/main/java/edu/phystech/hw3/SetUtils.java
@@ -0,0 +1,29 @@
+package edu.phystech.hw3;
+
+import java.util.Set;
+import java.util.*;
+
+public class SetUtils {
+    public static <E> Set<E> union(Set<? extends E> s1, Set<? extends E> s2) {
+        Set<E> result = new HashSet<>(s1);
+        result.addAll(s2);
+        return result;
+    }
+
+    public static <E> Set<E> intersection(Set<? extends E> s1, Set<? extends E> s2) {
+        Set<E> result = new HashSet<>(s1);
+        result.retainAll(s2);
+        return result;
+    }
+
+    public static <E> Set<E> difference(Set<? extends E> s1, Set<? extends E> s2) {
+        Set<E> result = new HashSet<>(s1);
+        result.removeAll(s2);
+        return result;
+    }
+
+    public static <E> Set<E> symmetricDifference(Set<? extends E> s1, Set<? extends E> s2) {
+        return union(difference(s1, s2), difference(s2, s1));
+    }
+
+}
---
+++ java-homework-main/src/test/java/edu/phystech/hw3/InverseMapKeyValuesTest.java
@@ -0,0 +1,61 @@
+package edu.phystech.hw3;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InverseMapKeyValuesTest {
+
+    public static <K, V> Map<V, Collection<K>> inverse(Map<? extends K, ? extends V> map) {
+        Map<V, Collection<K>> invertedMap = new HashMap<>();
+
+        for (Map.Entry<? extends K, ? extends V> entry : map.entrySet()) {
+            V value = entry.getValue();
+            Collection<K> keys = invertedMap.getOrDefault(value, new HashSet<>());
+            keys.add(entry.getKey());
+            invertedMap.put(value, keys);
+        }
+
+        return invertedMap;
+    }
+
+    @Test
+    void noDuplicateValuesTest() {
+        Map<Integer, String> originalMap = Map.of(1, "one", 2, "two");
+        Map<String, Collection<Integer>> expectedMap = Map.of("one", List.of(1), "two", List.of(2));
+
+        Map<String, Collection<Integer>> actualMap = inverse(originalMap);
+
+        Assertions.assertEquals(expectedMap.size(), actualMap.size());
+
+        expectedMap.forEach((key, value) -> {
+            var entry = actualMap.get(key);
+            Assertions.assertNotNull(entry);
+            Assertions.assertEquals(new HashSet<>(value), new HashSet<>(entry));
+        });
+
+    }
+
+    @Test
+    void duplicateValuesTest() {
+        Map<Integer, String> originalMap = Map.of(1, "one", 2, "two", 3, "two");
+        Map<String, Collection<Integer>> expectedMap = Map.of("one", List.of(1), "two", List.of(2, 3));
+
+        Map<String, Collection<Integer>> actualMap = inverse(originalMap);
+
+        Assertions.assertEquals(expectedMap.size(), actualMap.size());
+
+        expectedMap.forEach((key, value) -> {
+            var entry = actualMap.get(key);
+            Assertions.assertNotNull(entry);
+            Assertions.assertEquals(new HashSet<>(value), new HashSet<>(entry));
+        });
+
+    }
+}
+
